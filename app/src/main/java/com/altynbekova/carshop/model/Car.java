459286ff98c3c5329945861af05b399b536ac2ff
altynbekova <gulzhanaltynbekova@gmail.com>
2026-04-21 18:40:12 +0700

    initial commit

---
+++ app/src/main/java/com/altynbekova/carshop/model/Car.java
@@ -0,0 +1,58 @@
+package com.altynbekova.carshop.model;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private String description;
+    private int cost;
+    private String photo;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
